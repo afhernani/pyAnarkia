54f1defe41ae9bcd747de178a8152a5eb3db4206
hernani Aleman Ferraz <afhernani@hotmail.com>
2019-03-27 14:10:34 +0000

    Ajuste de formato

--- Tareas/bloqueos_tres.py
+++ Tareas/bloqueos_tres.py
@@ -9,23 +9,27 @@ llaman los hilos son equivalentes:
 
 import threading
 
+
 def con_with(bloqueo):
-	with bloqueo:
-		print('Bloqueo adquirido con with')
-		print('Procesando...')
+    with bloqueo:
+        print('Bloqueo adquirido con with')
+        print('Procesando...')
+
 
 def sin_with(bloqueo):
-	bloqueo.acquire()
-	try:
-		print('Bloqueo adquirido directamente')
-		print('procesando...')
-	finally:
-		bloqueo.release()
+    bloqueo.acquire()
+    try:
+        print('Bloqueo adquirido directamente')
+        print('procesando...')
+    finally:
+        bloqueo.release()
+
 
 def main():
-	bloqueo = threading.Lock()
-	hilo1 = threading.Thread(target=con_with, args=(bloqueo,))
-	hilo2 = threading.Thread(target=sin_with, args=(bloqueo,))
+    bloqueo = threading.Lock()
+    hilo1 = threading.Thread(target=con_with, args=(bloqueo,))
+    hilo2 = threading.Thread(target=sin_with, args=(bloqueo,))
+
 
 if __name__ == '__main__':
-	main()
+    main()
